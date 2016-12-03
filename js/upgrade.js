399b4d7c9926fa5f873f16a3df6522da10dabd33
jdossowski <johndean76@gmail.com>
2016-12-03 13:19:34 -0800

    Committing my bootstrap and js upgrade project.

---
+++ js/upgrade.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('[data-toggle="tooltip"]').tooltip();	
+});
+
+/* ORIGINALLY, I USED A POPOVER FOR TERMS AND CONDITIONS, BUT DECIDED
+I WANTED A MODAL INSTEAD.
+
+$(document).ready(function(){
+	$('[data-toggle="popover"]').popover();	
+});
+*/
